a73aafb3a59310af586346bda4646e59480827a8
Elizabeth J <elizabeth@wondertwinsventures.com>
2018-10-22 05:15:28 -0400

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,5 +1,11 @@
 class DoctorsController < ApplicationController
 	def index
 		@doctors = Doctor.all
+		#TODO the count of the doctor's patients
+	end
+
+	def show
+		@doctor = Doctor.find(params[:id])
+		
 	end
 end
